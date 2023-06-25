7cfeb33a4e7766ac53a330f29b3e4ddc395adaa6
nanpangyou <yigehd@foxmail.com>
2023-06-25 17:57:48 +0800

    add hook file

---
+++ src/hooks/useSwipe.tsx
@@ -0,0 +1,7 @@
+// const { deriction}  =  useSwipe(DIV)
+
+import { useState } from "react";
+
+export const useSwipe = (el: HTMLElement) => {
+  const [direction, setDirection] = useState("right");
+};
